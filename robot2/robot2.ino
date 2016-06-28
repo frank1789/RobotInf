b8226f7c093892f11e83b8c633d41828c8cd7ee6
frank1789 <Francesco@MacBook-Pro-di-Francesco.local>
2016-06-28 12:10:33 +0200

    review
    gli incroci non vanno ancora

--- robot2/robot2.ino
+++ robot2/robot2.ino
@@ -37,76 +37,162 @@ void setup()
 void loop()
 {
   stateMachine.update();
-  delay(2);
 }
 
-void idle () {
-  Serial.println("Stato IDLE");
-  motorDX.stop();
-  motorSX.stop();
-  Serial.println("motori fermi");
+void idle () 
+{
+	Serial.println("Stato IDLE");
+	motorDX.stop();
+	motorSX.stop();
+	Serial.println("motori fermi");
+/*
+	if(Serial.available()) 
+	{	
 
-  stateMachine.transitionTo(&Line_Follower);
+		char ch;
+		ch = Serial.read();
+		static unsigned long buf = 0;
+		if(ch >= '0' && ch <= '9')
+		{
+			buf = buf * 10 + ch - '0';
+		}
+
+		else 
+		{
+			command_parsing(ch, buf);
+			buf = 0;
+		}
+	}
+*/	
+	stateMachine.transitionTo(&Line_Follower);
 }
 
-void start() {
-  stateMachine.transitionTo(&Idle);
+void start() 
+{
+	delay(200);
+	Serial.println("Ciao! Sono in attesa di un comando:");
+	Serial.println("puoi usare:");
+	Serial.println(" l -> per partire con il line follower");
+	Serial.println(" d -> per girare a destra");	
+	Serial.println(" a -> per girare a sinistra");
+	Serial.println(" s -> per fermarti");
+	stateMachine.transitionTo(&Idle);
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-void line_follower(){
-  Serial.println("Stato Line_Follower");
-
-  uint8_t motorSpeed = 80;
-
-  //lettura dal sensore a ultrasuoni
-  float dist_obs = ultraSensor.distanceCm();
-  int last_error = 0;
-  //verifica costantemente se ci sono oggetti sul percorso e segue la linea se ci sono ostacoli torna in Idle
-  if (check_obstacle (dist_obs) == true){
-    //lettura posizione dai sensori di linea
-    int error = path_error(read_path(lineFinderDX.readSensors(), lineFinderSX.readSensors()));
-    if (error >= -4 && error <= 4)
-    {
-      int correction= 8 * error + 4 * (error - last_error);
-      last_error = error;
-      motorDX.run(-motorSpeed - correction);
-      motorSX.run( motorSpeed - correction);
-    }
-    else if(error == 5)
-    {
-      stop();
-      
-      motorDX.run(motorSpeed);
-      motorSX.run(motorSpeed);
-    }
-    else if (error == 6)
-    {
-      //stop();
-      if(crossbreed() == 1){
-        motorDX.run(motorSpeed);
-        //motorSX.run(motorSpeed);
-      }
-      else
-      {
-        //motorDX.run(-motorSpeed);
-        motorSX.run(-motorSpeed);
-      }
-
-    }
-  }
-else
+void line_follower()
 {
-  stop();
-      
-      motorDX.run(motorSpeed);
-      motorSX.run(motorSpeed);
+	Serial.println("Stato Line_Follower");
+
+	uint8_t motorSpeed = 100;
+
+	int last_error = 0;
+	const int Kp =	8;
+	const int Kd = 4;
+
+
+	//lettura dal sensore a ultrasuoni
+	float dist_obs = ultraSensor.distanceCm();
+	//verifica costantemente se ci sono oggetti sul percorso e segue la linea se ci sono ostacoli torna in Idle
+	if (check_obstacle (dist_obs) == true)
+	{
+		//lettura posizione dai sensori di linea
+		int error = path_error(read_path(lineFinderDX.readSensors(), lineFinderSX.readSensors()));
+		if (error >= -4 && error <= 4)
+		{
+			int correction = Kp * error + Kd * (error - last_error);
+			last_error = error;
+			motorDX.run(-motorSpeed - correction);
+			motorSX.run( motorSpeed - correction);
+		}
+
+		else if (error == 5)
+		{
+			motorDX.run(motorSpeed);
+			motorSX.run(motorSpeed);
+		}	
+
+		else if  (error == 6)
+		{	
+			stop();
+			delay(200);
+		 	motorDX.run(80);
+		 	motorSX.run(10);
+		// 	//stateMachine.transitionTo(&Idle);
+		}
+	}
+	
+	else
+	{
+		stop();
+		motorDX.run(motorSpeed);
+		motorSX.run(motorSpeed);
 	}
 }
 
 void stop(){
 	Serial.println("Stato STOP");
-  motorDX.stop();
-  motorSX.stop();
+	motorDX.stop();
+	motorSX.stop();
+}
+
+void command_parsing(char ch, unsigned long val) 
+{
+	Serial.println("Ciao! Sono in attesa di un comando:");
+	Serial.println("puoi usare:");
+	Serial.println(" l -> per partire con il line follower");
+	Serial.println(" d -> per girare a destra");	
+	Serial.println(" a -> per girare a sinistra");
+	Serial.println(" s -> per fermarti");
+	switch (ch) 
+	{
+		case 's': //stop
+			Serial.println("success s");
+			stateMachine.transitionTo(&Stop);
+			// if(val > settings->min) 
+			// {
+			// 	settings->max = val;
+			// }
+			// PROMPT;
+		break;
+
+		// case 'i': //idle
+		// 	Serial.println("success i");
+		// 	// if(val < settings->max)
+		// 	// {
+		// 	// 	settings->min = val;
+		// 	// }
+		// 	// PROMPT;
+		// break;
+
+		case 'l': //start line follower
+			Serial.println("success l");
+			stateMachine.transitionTo(&Line_Follower);
+			// settings->time_const = val;
+			// PROMPT;
+		break;
+
+		case 'd': //destra
+			Serial.println("success d");
+			motorDX.run(-80);
+			motorSX.run(40);
+			stateMachine.transitionTo(&Line_Follower);
+			// settings->blink_period = val;
+			// PROMPT;
+		break;
+
+		case 'a': //sinistra
+			Serial.println("success a");
+			motorSX.run(80);
+			motorDX.run(-40);
+			stateMachine.transitionTo(&Line_Follower);
+			// if(state_vars.stateBeforeIdle != &Idle)
+			// {
+			// 	state_vars.stateBeforeIdle = stateMachine.getCurrentState();
+			// }
+			// stateMachine.transitionTo(&Idle);
+			// PROMPT;
+		break;
+	}
 }
