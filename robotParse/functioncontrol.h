637005aad24e0f202f1d218f255bc14d20dccb93
frank1789 <Francesco@MacBook-Pro-di-Francesco.local>
2016-06-30 09:17:22 +0200

    RobotParse
    -Parsing of command by serial
    -test in Lab
    -based on robot2 (lab 29/06)

---
+++ robotParse/functioncontrol.h
@@ -0,0 +1,10 @@
+#ifndef __FUNCTIONCONTROL_H
+#define __FUNCTIONCONTROL_H
+#include "input_data.h"
+
+//valuta l'errore sul percoso 
+int path_error (int path);
+//messaggi per l'utente
+void message();
+  
+#endif
